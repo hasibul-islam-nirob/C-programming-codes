ec58755fa93f825e061b4e5075800e5de00f6493
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2020-03-29 14:55:30 +0600

    Add files via upload

---
+++ sum of prime Number .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int m,n,p,i,j,chack,sum;
+
+    scanf("%d %d",&m,&n);
+    sum=0;
+    for(i=m; i<=n; i++){
+        p=i;
+        chack = 1;
+        if(p==0){
+            chack = 0;
+            }
+
+        for(j=2; j<p; j++){
+            if(p%j==0){
+                chack = 0;
+                }
+        }
+        if(chack==1){
+            sum = sum+p;
+            }
+    }
+    printf("%d\n",sum);
+
+return 0;
+}
---
+++ sum of prime Number 3 .c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int m,n,p,i,j,chack,sum,temp;
+
+    scanf("%d %d",&m,&n);
+    sum=0;
+    if(m>n){
+        temp = m;
+        m = n;
+        n = temp;
+    }
+
+        for(i=m; i<=n; i++){
+            p=i;
+            chack = 1;
+            if(p==0){
+                chack = 0;
+                }
+
+            for(j=2; j<p; j++){
+                if(p%j==0){
+                    chack = 0;
+                    }
+            }
+            if(chack==1){
+                sum = sum+p;
+                }
+        }
+    printf("%d\n",sum);
+
+return 0;
+}
